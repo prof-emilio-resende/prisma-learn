d77103296cacb79fc85cbc5fe093eeb2b17c2b40
Emilio Resende <emilio.murta@msitbrasil.com>
2024-08-05 05:12:11 -0300

    initial commit

---
+++ prisma/migrations/20240805081002_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "name" TEXT
+);
